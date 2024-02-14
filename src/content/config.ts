9d8c6c46dc370a20b648bad2a46c92c446eee672
leroy <16562492+leroy009@users.noreply.github.com>
2024-02-14 15:54:39 +0200

    defined the blog collection and schema.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,17 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 
-const blogCollection = defineCollection({});
+const blogCollection = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		publishedDate: z.string(),
+		author: z.string(),
+		imgae: z.string(),
+		tags: z.array(z.string()),
+		photographer: z.string(),
+		imageurl: z.string(),
+	}),
+});
 
 export const collections = {
 	blog: blogCollection,
