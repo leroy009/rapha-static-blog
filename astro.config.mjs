4662ed91f7fecaf90572c2dd9c4fcf5af380c170
leroy <16562492+leroy009@users.noreply.github.com>
2024-02-19 18:48:45 +0200

    added cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon(), tailwind()],
-	output: "server",
+  integrations: [icon(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
